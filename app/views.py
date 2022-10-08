7e65b836b864862b2daab7c418ea7cf778254e57
Muskan Marwah <marwahmuskan330@gmail.com>
2022-10-08 19:30:01 +0530

    [Muskan] Added /ping api

--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+import json
 
 # Create your views here.
+
+@api_view(('GET',))
+def ping(request):
+    return Response({"data": "pong"})
